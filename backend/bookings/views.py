f8aa87b501ae1d3c8b911964b56830394a44bd5f
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-14 10:14:47 +0800

    create booking view

--- backend/bookings/views.py
+++ backend/bookings/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Booking
+from .serializers import BookingSerializer
 
-# Create your views here.
+
+class BookingViewSet(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=["patch"])
+    def cancel(self, request, pk=None):
+        booking = self.get_object()
+
+        if booking.status == "CANCELLED":
+            return Response({"detail": "Booking already cancelled."})
+
+        booking.status = "CANCELLED"
+        booking.save()
+
+        return Response({"detail": "Booking cancelled successfully"})

ba303b363c8bf80463e47080e383824322c8bf0f
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-14 10:16:25 +0800

    create court view

--- backend/courts/views.py
+++ backend/courts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Court
+from .serializers import CourtSerializer
 
-# Create your views here.
+
+class CourtViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Court.objects.filter(is_active=True)
+    serializer_class = CourtSerializer
+    permission_classes = [AllowAny]

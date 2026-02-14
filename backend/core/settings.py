996b703b3cd08adeca803e56e63aa4ff11a7a3e9
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-14 16:53:14 +0800

    add comma in rest_framework

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
 }
 
@@ -94,8 +94,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "badminton_booking_db",
+        "USER": "postgres",
+        "PASSWORD": "Paswud109",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
